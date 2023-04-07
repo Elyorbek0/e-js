cbb1a45930c9b86f0358feaff86ceddfe30294c6
faerumidovna <mushtariyumidovna1404@gmail.com>
2023-04-07 14:31:12 +0500

    fix

---
+++ script.js
@@ -0,0 +1,28 @@
+// ================== 1 misol =====================
+// let a = +prompt('a sonni kiriting')
+// let b = +prompt('b sonni kiriting')
+// let c = +prompt('c sonni kiriting')
+
+// ================== 2 misol =====================
+// let a = +prompt('niqoblar sonini kiriting')
+// let b = +prompt('odamlar sonini kiriting')
+
+
+
+// ================== 3 misol =====================
+
+// var arr = prompt("raqam kiriting")
+
+// alert(arr[0])
+
+
+// ================== 4 misol =====================
+
+// let a = +prompt('a raqamini kiriting')
+// let b = +prompt('b raqamini kiriting')
+
+// function and(a,b){
+//    return(b+ " va " +a)
+// }
+
+// alert(and(a,b))
